9267db7414a02d57f4b3d6258777c40652bd154d
Toby Curtis <toby.curtis@revcult.com>
2022-07-22 22:30:44 -0700

    COMMIT_MESSAGE

--- dev-tools/do-it-live.sh
+++ dev-tools/do-it-live.sh
@@ -1,8 +1,17 @@
+
+COMMIT_MESSAGE
+
+if [ -z "$1" ]; then
+  COMMIT_MESSAGE="doin it live"
+else 
+  COMMIT_MESSAGE="$1"
+fi
+
 echo "building"
 npm run build
 echo "building"
 git add .
 echo "building"
-git commit -m "doin it live"
+git commit -m COMMIT_MESSAGE
 echo "building"
 git push heroku master
